77bcc1ef464a8dadd87c5444eed92be5786aff7d
Nosyt118 <ty123hud@gmail.com>
2024-05-12 11:02:02 -0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Playing .gg/ohiovc","Watching over Aqxrea, Makign Aqxrea lunch"];
 
 
 let currentIndex = 0;
